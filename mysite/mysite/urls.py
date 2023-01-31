fb5533af7c7bebdf35963e9d98564b6c6974ecf6
pinipv <adrianpinillapf@gmail.com>
2023-01-31 23:37:51 +0100

    Inicio

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from .views import views
+
 
 app_name = 'polls'
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-    path('specifics/<int:question_id>/', views.detail, name= 'detail')
+    path('admin/', admin.site.urls)
+    
 ]
---
+++ mysite/polls/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,get_object_or_404
+from django.template import loader
+from django.http import HttpResponse, Http404
+
+from .models import Question
+
+
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        
+        return Question.objects.filter(
+                    pub_date__lte = timezone.now()
+                ).order_by('pub_date')[:5]
+        
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    ... # same as above, no changes needed.
